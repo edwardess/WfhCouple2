9abca57de044c8bba8f92a2e1221e44c79760c12
edwardess <edabdu25@gmail.com>
2024-10-10 10:13:44 +0800

    adjusted middleware

--- middleware.ts
+++ middleware.ts
@@ -2,15 +2,15 @@ import { authMiddleware } from "@clerk/nextjs";
 import { NextResponse } from 'next/server';
 
 export default authMiddleware({
-  publicRoutes: ["/", "/api/webhook"],
+  publicRoutes: ["/", "/api/webhook", "/portfolio"],  // Add /portfolio to public routes
 
   afterAuth: (auth, req) => {
     const protectedRoutes = ['/dashboard', '/search', '/leaderboard', '/shop', '/settings'];
 
-    console.log("Auth Status:", auth);  // Debugging the auth object returned from Clerk
-    console.log("User ID:", auth.userId);  // Check if userId is correctly detected
-    console.log("Session ID:", auth.sessionId);  // Check if a session ID is present
-    console.log("Route being accessed:", req.nextUrl.pathname);  // Log the route being accessed
+    console.log("Auth Status:", auth);
+    console.log("User ID:", auth.userId);
+    console.log("Session ID:", auth.sessionId);
+    console.log("Route being accessed:", req.nextUrl.pathname);
 
     if (!auth.userId && protectedRoutes.includes(req.nextUrl.pathname)) {
       console.log("No user authenticated, redirecting to sign-in.");
