33353de1118bc42d4ba71aebc3203287b2b8fb96
edwardess <edabdu25@gmail.com>
2024-10-10 13:38:22 +0800

    Initial commit

--- middleware.ts
+++ middleware.ts
@@ -4,9 +4,10 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/", '/api/webhook', "/portfolio", "/portfolio/:path*", "/dashboard", "/settings", "/sign-in", "/sign-up"],
+  publicRoutes: ["/", '/api/webhook',]
 });
 
 export const config = {
-  matcher: ['/((?!.*\.[\w]+$|_next|portfolio).*)', '/', '/(api|trpc)(.*)', '/dashboard', '/settings', '/sign-in', '/sign-up'],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+ 
