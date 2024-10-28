dcb03f27a19a762afdfbfb175de7d402957cd537
edwardess <89014139+edwardess@users.noreply.github.com>
2024-10-28 13:07:20 +0800

    Update consultation.js

--- app/api/consultation.js
+++ app/api/consultation.js
@@ -2,9 +2,18 @@
 import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
-  const response = await fetch('https://officialwfhcouple.systeme.io/consultation');
-  const data = await response.text();
+  try {
+    const response = await fetch('https://officialwfhcouple.systeme.io/consultation');
+    
+    if (!response.ok) {
+      res.status(response.status).end('Error fetching the page');
+      return;
+    }
 
-  res.setHeader('Content-Type', 'text/html');
-  res.send(data);
+    const data = await response.text();
+    res.setHeader('Content-Type', 'text/html');
+    res.send(data);
+  } catch (error) {
+    res.status(500).send('Server error');
+  }
 }
